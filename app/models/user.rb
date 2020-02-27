128b96ae7b03420721dfcdf3895bd133e6b2a3dc
Aditi <prateek@akaruilabs.com>
2020-02-27 11:17:12 +0530

    sample app with validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User <  ActiveRecord::Base
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 200 }, format: { with: VALID_EMAIL_REGEX },
+  validates :email, presence: true, length: { maximum: 200 },
   uniqueness: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
